d516990cffa7905f2ce7725c40ca99df40a61019
Pavel <Pavel@SILENCIO>
2016-02-22 22:29:16 +0400

    Till 21

--- IO/src/com/kostenko/io/e17.java
+++ IO/src/com/kostenko/io/e17.java
@@ -5,10 +5,30 @@
  */
 package com.kostenko.io;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+import net.mindview.util.TextFile;
+
 /**
  *
  * @author Pavel
  */
 public class e17 {
-    
+
+    static char[] chars = TextFile.read("d:\\temp\\test.txt").toCharArray();
+    static HashMap<Character, Integer> letterFreq
+            = new HashMap<Character, Integer>();
+
+    public static void main(String[] args) {
+        for (char c : chars) {
+            if (letterFreq.get(c) != null) {
+                letterFreq.put(c, letterFreq.get(c) + 1);
+            } else {
+                letterFreq.put(c, 1);
+            }
+        }
+        System.out.println(letterFreq);
+    }
 }
